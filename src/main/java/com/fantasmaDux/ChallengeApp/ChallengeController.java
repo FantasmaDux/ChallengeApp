453578e54d3604d3687544d9cc801be3eda7af41
FantasmaDux <gdgdgdgdsd@mail.ru>
2025-06-23 20:52:48 +0300

    feat(update): add update record feature

--- src/main/java/com/fantasmaDux/ChallengeApp/ChallengeController.java
+++ src/main/java/com/fantasmaDux/ChallengeApp/ChallengeController.java
@@ -1,9 +1,8 @@
 package com.fantasmaDux.ChallengeApp;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,5 +24,14 @@ public class ChallengeController {
     boolean isChallengeAdded = challengeService.addChallenge(challenge);
     if (isChallengeAdded) return "Challenge added successfully";
     else return "Challenge not added successfully";
+  }
+
+  @PutMapping("/challenges/")
+  public ResponseEntity<String> updateChallenge(@PathVariable Long id, @RequestBody Challenge updatedChallenge) {
+    boolean isChallengeUpdated = challengeService.updateChallenge(id, updatedChallenge);
+    if (isChallengeUpdated)
+      return new ResponseEntity<>("Challenge updated successfully", HttpStatus.OK);
+    else
+      return new ResponseEntity<>("Challenge not updated successfully", HttpStatus.BAD_REQUEST);
   }
 }
